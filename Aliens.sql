74ef7b6f7c638662729fcd54b1578ea0262191b2
Vince Chang <vincechangsf@gmail.com>
2020-04-15 12:39:41 -0700

    Adding alien sql

---
+++ Aliens.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE restdb;
+
+CREATE TABLE alien(id int, name varchar(20), points int);
+
+SELECT * FROM alien;
+
+INSERT INTO alien VALUES(1, "baloo", 300);
+
+DELETE FROM alien WHERE id=4;
